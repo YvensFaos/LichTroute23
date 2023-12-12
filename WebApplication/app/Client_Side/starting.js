9667fd13062544ad684bfd5efe0600beb7074d06
JJ <jjrosenberg122@gmail.com>
2023-12-12 16:47:32 +0100

    Replace the file path of instrument page

--- WebApplication/app/Client_Side/starting.js
+++ WebApplication/app/Client_Side/starting.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const startButton = document.getElementById("startButton");
     startButton.addEventListener('click', function (event) {
-        window.location.href='../index.html';
+        window.location.href='/app/customize.htmls';
     });
 
 
